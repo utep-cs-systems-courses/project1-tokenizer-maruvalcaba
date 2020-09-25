bb3ca179c63ac9e8c02f32247031378e42f41592
maruvalcaba <maruvalcaba@miners.utep.edu>
2020-09-25 11:11:12 -0600

    Added comments to uimain.c

--- src/uimain.c
+++ src/uimain.c
@@ -4,49 +4,49 @@
 #include "history.h"
 int main()
 {
-  List *list = init_history();
+  List *list = init_history();  /* Initializes the list */
   while(1){
     printf("Please enter a sentence to tokenize, or use the following commands:\n!q to quit, !n where n is an id in the history to tokenize that string, !h to view the history.\n\n");
     printf("> ");
-    char str[50];
-    scanf(" %[^\n]", str, 50);
+    char str[200];
+    scanf(" %[^\n]", str, 200);
     puts("\n\n");
-    if(str[0] == '!'){
-	if(str[1] == 'q'){
-	    goto quit;
-	  }
-	else if(str[1] == 'h'){
-	    print_history(list);
-	    puts("\n\n");
-	  }
-	else{
-	    char strNum[49];
-	    int z = 0;
-	    while(str[z+1]){
-		strNum[z] = str[z+1];
-		z++;
-	      }
-	    int id = atoi(strNum);
-	    char *str1 = get_history(list, id);
-	    if(str1 == 0){
-		printf("Invalid input.\n");
-		continue;
-	      }
-	    char **tokenized = tokenize(str1);
-	    printf("%s\n\n", str1);
-	    print_tokens(tokenized);
-	    puts("\n\n");
-	    free_tokens(tokenized);
-	    continue;
-	  }
+    if(str[0] == '!'){    /* if the input is a command */
+      if(str[1] == 'q'){       /* if the command is quit, go to quit */
+	goto quit;
       }
-    else{
-	add_history(list, str);
-	char **tokenized = tokenize(str);
+      else if(str[1] == 'h'){  /* if the command is view history, print the history */
+	print_history(list);
+	puts("\n\n");
+      }
+      else{                    /* if the command is to tokenize a history item */
+	char strNum[199];
+	int z = 0;
+	while(str[z+1]){
+	  strNum[z] = str[z+1];
+	  z++;
+	}
+	int id = atoi(strNum);
+	char *str1 = get_history(list, id);
+	if(str1 == 0){
+	  printf("Invalid input.\n");
+	  continue;
+	}
+	char **tokenized = tokenize(str1);  /* the string is tokenized */
+	printf("%s\n\n", str1);
 	print_tokens(tokenized);
 	puts("\n\n");
 	free_tokens(tokenized);
+	continue;
       }
+    }
+    else{                    /* otherwise the input is a string to tokenize, and is tokenized. */
+      add_history(list, str);
+      char **tokenized = tokenize(str);
+      print_tokens(tokenized);
+      puts("\n\n");
+      free_tokens(tokenized);
+    }
   } 
  quit:
   printf("Goodbye!\n");
