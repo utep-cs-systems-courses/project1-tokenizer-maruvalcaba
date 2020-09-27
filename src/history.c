737d05e5324c605a7e9e6ae2c7ff918c471f5042
maruvalcaba <maruvalcaba@miners.utep.edu>
2020-09-27 16:34:48 -0600

    Added comments to history.c

--- src/history.c
+++ src/history.c
@@ -8,33 +8,33 @@ List *init_history()                  /* initializes the history list of tokens,
   return history;
 }
 
-void add_history(List *list, char *str)
+void add_history(List *list, char *str) /* adds an item to the history */
 {
   int itemid = 1;
   if((list->root) == 0){
-      Item *newItem = malloc(sizeof(Item));
-      newItem -> id = itemid;
-      newItem -> next = 0;
-      char *sc = str;
-      while(*sc++);
-      int strindex = 0;
-      char *strcopy = malloc((sc-str+1) * sizeof(char));
-      char c;
-      do
-	{
-	  c = (*(strcopy+strindex) = *(str+strindex));
-	  strindex++;
-	} while (c);
-      newItem->str = strcopy;
-      list->root = newItem;
-    }
+    Item *newItem = malloc(sizeof(Item)); /* allocates the space for the new node */
+    newItem -> id = itemid;
+    newItem -> next = 0;
+    char *sc = str;
+    while(*sc++);
+    int strindex = 0;
+    char *strcopy = malloc((sc-str+1) * sizeof(char)); /* allocates the space for a string copy */
+    char c;
+    do
+      {
+	c = (*(strcopy+strindex) = *(str+strindex));
+	strindex++;
+      } while (c);
+    newItem->str = strcopy;
+    list->root = newItem;
+  }
   else{
     itemid++;
     Item *current = list->root;
-    while(current->next){
-	current = current->next;
-	itemid++;
-      }
+    while(current->next){                 /* while there is still a next item */
+      current = current->next;
+      itemid++;
+    }
     Item *newItem = malloc(sizeof(Item));
     newItem -> id = itemid;
     newItem -> next = 0;
@@ -53,37 +53,39 @@ void add_history(List *list, char *str)
   }
 }
 
-char *get_history(List *list, int id){
+char *get_history(List *list, int id)  /* returns an item in the history */
+{
   Item *current = list->root;
   while(current){
-      if((current->id) == id){
-	  return current->str;
-	}
-      else{
-	  current = current->next;
-	}
+    if((current->id) == id){
+      return current->str;
     }
+    else{
+      current = current->next;
+    }
+  }
   return 0;
 }
 
-void print_history(List *list){
+void print_history(List *list)        /* prints the entire contents of the history */
+{
   Item *current = list->root;
   while(current != 0){
-      printf("(%d) %s\n", (current->id), (current->str));
-      current = current->next;
-    }
+    printf("(%d) %s\n", (current->id), (current->str));
+    current = current->next;
+  }
 }
 
-void free_history(List *list)
+void free_history(List *list)        /* frees the memory used by the entire history */
 {
   Item *current = list->root;
   Item *next;
   while(current){
-      next = current->next;
-      free(current->str);
-      free(current);
-      current = next;
-    }
+    next = current->next;
+    free(current->str);
+    free(current);
+    current = next;
+  }
   free(list);
 }
 
